e6e6421c4cdc81b75966f37c4531d69ff4641cec
HARRYnoob <pravendraaset01@gmail.com>
2025-11-13 17:29:05 +0530

    added tab button.

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ const button = document.getElementById("btn");
 const input = document.getElementById("typeBox");
 const output = document.getElementById("divv");
 const button2 = document.getElementById("buton");
+const saveTab = document.getElementById("save-tab");
 const moveInfo = JSON.parse(localStorage.getItem("info"));
-console.log(moveInfo);
+//console.log(moveInfo);
 let savedInfo  = [];
 if (moveInfo){
     savedInfo = moveInfo;
     render(savedInfo);
 }
+const tab = [{url : "https://www.instagram.com/"}]
+saveTab.addEventListener("click",function(){
+savedInfo.push(tab[0].url);
+localStorage.setItem("info",JSON.stringify(savedInfo))
+render(savedInfo);
+})
+
 button2.addEventListener("dblclick",function(){
 localStorage.clear();
 savedInfo = [];
 render(savedInfo);
 })
 button.addEventListener("click",function(){
-    savedInfo.push(input.value);
+    if (input.value){savedInfo.push(input.value)};
     localStorage.setItem("info", JSON.stringify(savedInfo))
     render(savedInfo)
 })
